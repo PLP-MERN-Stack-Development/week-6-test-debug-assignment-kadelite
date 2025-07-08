95f1ba9ff93a0bb2d2768b32b3fc3dd08412202a
kadelite <mechatronics.samson@gmail.com>
2025-07-08 22:26:07 +0100

    fix: Button.jsx fixed

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-const Button = ({ children, onClick, disabled }) => (
-  <button onClick={onClick} disabled={disabled} data-testid="button">
-    {children}
-  </button>
-);
+const Button = ({
+  children,
+  onClick,
+  disabled,
+  variant = 'primary',
+  size = 'md',
+  className = '',
+  ...rest
+}) => {
+  const classes = [
+    `btn-${variant}`,
+    `btn-${size}`,
+    disabled ? 'btn-disabled' : '',
+    className
+  ]
+    .filter(Boolean)
+    .join(' ');
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={classes}
+      data-testid={rest['data-testid'] || 'button'}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button; 
